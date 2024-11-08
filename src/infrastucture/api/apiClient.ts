5d13f75e0bc97979c004b7f5151f6c58c85a034a
kevin-lo-al-98 <kevinlopezalvarez98@gmail.com>
2024-11-08 02:38:45 -0500

    Add configurable baseUrl in apiClient for flexible API endpoint management"

--- src/infrastucture/api/apiClient.ts
+++ src/infrastucture/api/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "https://ca2b1a421f421769bde5.free.beeceptor.com", // Cambia la URL según tu servidor
+  baseURL: "https://ca19855df6b2962a89d6.free.beeceptor.com", // Cambia la URL según tu servidor
   headers: {
     "Content-Type": "application/json",
   },
